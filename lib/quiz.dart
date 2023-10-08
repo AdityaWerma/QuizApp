5cd87e01bcc8b28c47b1b8776843b2f96746e47a
aditya kumar <akv.small.api@gmail.com>
2023-10-08 10:05:46 +0530

    Passing data via functions across widgets

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -12,12 +12,16 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  final List<String> selectedAnswers = [];
   var activeScreen = 'start-screen';
 
   void switchScreen() {
     setState(() {
       activeScreen = 'questions-screen';
     });
+  }
+  void chooseAnswer(String answer){
+    selectedAnswers.add(answer);
   }
 
   @override
@@ -35,7 +39,7 @@ class _QuizState extends State<Quiz> {
         ),
         child: activeScreen == 'start-screen'
             ? StartScreen(switchScreen)
-            : const QuestionsScreen(),
+            : QuestionsScreen(onSelectAnswer: chooseAnswer),
       )),
     );
   }
